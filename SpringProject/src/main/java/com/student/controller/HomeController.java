1eb332584e6332b08ab86d37d40c493311341dee
SIMRAN RATHORE <simranrathore63@gmail.com>
2018-09-01 17:17:31 +0530

    first commit

---
+++ SpringProject/src/main/java/com/student/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.student.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+@RequestMapping("/")
+public ModelAndView getIndexPage() {
+	ModelAndView mv=new ModelAndView("home");
+	
+	return mv;
+}
+}
