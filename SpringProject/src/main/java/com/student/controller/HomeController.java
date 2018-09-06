f0fdf71d55aebf0aba539552b2e3835f75e99b0b
SIMRAN RATHORE <simranrathore63@gmail.com>
2018-09-06 14:19:53 +0530

    update

--- SpringProject/src/main/java/com/student/controller/HomeController.java
+++ SpringProject/src/main/java/com/student/controller/HomeController.java
@@ -1,8 +1,16 @@
 package com.student.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.student.verify.VerifyRecaptcha;
 
 @Controller
 public class HomeController {
@@ -10,5 +18,31 @@ public class HomeController {
 	public ModelAndView getIndexPage() {
 		ModelAndView mv = new ModelAndView("home");
 		return mv;
+	}
+
+	@RequestMapping(value = "/LoginServlet", method = RequestMethod.POST)
+	public ModelAndView getServletPage(HttpServletRequest request, HttpServletResponse response) {
+
+		String user = request.getParameter("user");
+		String pwd = request.getParameter("pwd");
+		// get reCAPTCHA request param
+		String gRecaptchaResponse = request.getParameter("g-recaptcha-response");
+		System.out.println(gRecaptchaResponse);
+		boolean verify = false;
+		try {
+			verify = VerifyRecaptcha.verify(gRecaptchaResponse);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		System.out.println("User=" + user + "::password=" + pwd + "::Captcha Verify " + verify);
+		if (verify) {
+			// response.sendRedirect("LoginSuccess");
+			ModelAndView mv = new ModelAndView("LoginSuccess");
+			return mv;
+		} else {
+			ModelAndView mv = new ModelAndView("home");
+			return mv;
+		}
+
 	}
 }
--- SpringProject/src/main/webapp/WEB-INF/home.jsp
+++ SpringProject/src/main/webapp/WEB-INF/home.jsp
@@ -1,12 +1,19 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
+<script src='https://www.google.com/recaptcha/api.js'></script>
 <title>Insert title here</title>
 </head>
 <body>
-<h1>Thank u Pawan</h1>
+	<form action="LoginServlet" method="post">
+
+		Username: <input type="text" name="user"> <br> Password:<input
+			type="password" name="pwd"> <br>
+		<div class="g-recaptcha"
+			data-sitekey="6LdtzW0UAAAAAKLQeKo5DALTFXPA7VtGQK-B6zbu"></div>
+		<br> <input type="submit" value="Login">
+	</form>
 </body>
 </html>
